89f38cc4030958e1fc4d2a5ae04884b2210144d8
Rafael da Silva Castro <rafaeldasilvacastroti@gmail.com>
2024-03-15 10:23:44 -0300

    funcionalidade de mostrar as tarefas e de deletar as tarefas

--- tarefas/src/pages/Admin/index.jsx
+++ tarefas/src/pages/Admin/index.jsx
@@ -4,17 +4,37 @@ import { useState, useEffect } from 'react'
 
 import { auth, db } from '../../firebaseConnection'
 import { signOut } from 'firebase/auth'
-import { addDoc, collection } from 'firebase/firestore'
+import { addDoc, collection, onSnapshot, query, orderBy, where, doc, deleteDoc } from 'firebase/firestore'
 
 function Admin() {
 
   const [tarefaInput, setTarefaInput] = useState('')
   const [user, setUser] = useState('')
+  const [tarefas, setTarefas] = useState([])
 
   useEffect(() => {
     async function loadTarefas() {
       const userDatail = localStorage.getItem('@detailUser')
       setUser(JSON.parse(userDatail))
+
+      if (userDatail) {
+        const data = JSON.parse(userDatail)
+
+        const tarefaRef = collection(db, 'tarefas')
+        const q = query(tarefaRef, orderBy('created', 'desc'), where('userUid', '==', data?.uid))
+        const unsub = onSnapshot(q, (snapshot) => {
+          let lista = []
+          snapshot.forEach((doc) => {
+            lista.push({
+              id: doc.id,
+              tarefa: doc.data().tarefa,
+              userUid: doc.data().userUid
+            })
+          })
+          console.log(lista)
+          setTarefas(lista)
+        })
+      }
     }
 
     loadTarefas();
@@ -36,17 +56,23 @@ function Admin() {
       created: new Date(),
       userUid: user?.uid,
     })
-    .then(()=>{
-      console.log('Tarefa cadastrada')
-      setTarefaInput('')
-    })
-    .catch((e)=>{
-      console.log('erro ao registrar '+e)
-    })
+      .then(() => {
+        console.log('Tarefa cadastrada')
+        setTarefaInput('')
+      })
+      .catch((e) => {
+        console.log('erro ao registrar ' + e)
+      })
   }
 
   async function handleLogout() {
     await signOut(auth);
+  }
+
+
+  async function deleteTarefa(id){
+    const docRef = doc(db, 'tarefas', id);
+    await deleteDoc(docRef)
   }
 
   return (
@@ -61,13 +87,15 @@ function Admin() {
           <button className='btn-register' type='submit'>Registrar tarefa</button>
         </form>
 
-        <article className='list'>
-          <p>estudar JS</p>
+        {tarefas.map((item)=>(
+          <article key={item.id} className='list'>
+          <p>{item.tarefa}</p>
           <div>
             <button>editar</button>
-            <button className='btn-delete'>concluir</button>
+            <button onClick={()=>{deleteTarefa(item.id)}} className='btn-delete'>concluir</button>
           </div>
         </article>
+        ))}
 
         <button onClick={handleLogout} className='btn-logout'>Sair</button>
 
